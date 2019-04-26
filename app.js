66ce95e7ce4db4015cd3b85a2f1ac5806c8e6efb
m43max <maxpt750@gmail.com>
2019-04-26 09:15:33 +0700

    last

--- app.js
+++ app.js
@@ -12,15 +12,31 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.static(path.join(__dirname, "public"))) //Set static folder
 
 app.use(session({
-    secret: 'alvin'
+    secret: 'alvinmaxtrivia',
+    resave: false,
+    saveUninitialized: true,
 }))
 
 app.get("/", (req, res) => {
-    res.render("home.ejs")
+
+    if(!req.session.loggedin) {
+        req.session.loggedin = false
+    }
+
+    console.log(`
+        ${JSON.stringify(req.session)}
+    `)
+    res.render("home.ejs", {
+        loggedin: req.session.loggedin,
+        email: req.session.email
+    })
 })
 
 app.get("/login", (req, res) => {
-    res.render("login.ejs")
+    res.render("login.ejs", {
+        loggedin: req.session.loggedin,
+        email: req.session.email
+    })
 })
 
 app.post("/login", (req, res) => {
@@ -35,7 +51,9 @@ app.post("/login", (req, res) => {
     .then(user => {
         if(!user) {
             res.render("print_msg.ejs", {
-                msg: "Email not registered"
+                msg: "Email not registered",
+                loggedin: req.session.loggedin,
+                email: req.session.email
             })
             return
         }
@@ -46,20 +64,42 @@ app.post("/login", (req, res) => {
             user: ${user}
         `)
         if(bcrypt.compareSync(info.password, user.password)) {
+
+
+            req.session.email = info.email;
+            req.session.password = info.password;
+            req.session.loggedin = true;
+
+            console.log(`
+            ${JSON.stringify(req.session)}
+            `)
+
             res.render("print_msg.ejs", {
-                msg: "Login Success"
+                msg: "Login Success",
+                loggedin: req.session.loggedin,
+                email: req.session.email
             })
         }
         else {
             res.render("print_msg.ejs", {
-                msg: "Login Failed"
+                msg: "Login Failed",
+                loggedin: req.session.loggedin,
+                email: req.session.email
             })
         }
     })
 })
 
+app.get("/logout", (req, res) => {
+    req.session.destroy();
+    res.redirect("/");
+})
+
 app.get("/register", (req, res) => {
-    res.render("register.ejs")
+    res.render("register.ejs", {
+        loggedin: req.session.loggedin,
+        email: req.session.email
+    })
 })
 
 app.post("/register", (req, res) => {
@@ -75,7 +115,9 @@ app.post("/register", (req, res) => {
     })
     .then(() => {
         res.render("print_msg.ejs", {
-            msg: "Successfully registered"
+            msg: "Successfully registered",
+            loggedin: req.session.loggedin,
+            email: req.session.email
         })
     })
     .catch(err => {res.send(err)})
@@ -83,26 +125,153 @@ app.post("/register", (req, res) => {
 })
 
 app.get("/topics", (req, res) => {
-    res.render("topics.ejs")
+    res.render("topics.ejs", {
+        loggedin: req.session.loggedin,
+        email: req.session.email
+    })
 })
 
 app.post("/topics/:topic", (req, res) => {
     let topic = req.params.topic
     Model.Topic.findOne()
-    res.render("topics.ejs")
+    res.render("topics.ejs", {
+        loggedin: req.session.loggedin,
+        email: req.session.email
+    })
 })
 
 app.get("/leaderboard", (req, res) => {
-    res.render("leaderboard.ejs")
+    res.render("leaderboard.ejs", {
+        loggedin: req.session.loggedin,
+        email: req.session.email
+    })
 })
 
 app.get("/account", (req, res) => {
-    res.render("account.ejs")
+    res.render("account.ejs", {
+        loggedin: req.session.loggedin,
+        email: req.session.email
+    })
 })
 
-app.get("/quiz:id", (req, res) => {
+app.get("/quiz/:id", (req, res) => {
     let qid = req.params.id
-    res.send(qid)
+    Model.Question.findAll({
+        where:{
+            TopicId: qid
+        },
+        include: [{model: Model.Answer}]
+    })
+    .then(data => {
+        // res.send(data)
+        res.render("quiz.ejs", {
+            data:data,
+            qid:qid,
+            loggedin: req.session.loggedin,
+            email: req.session.email
+        })
+    })
 })
+
+app.post("/quiz/:id", (req, res) => {
+  let qid = req.params.id
+  totalScore(req.body,qid)
+  .then(score=>{
+    return Model.UserTopic.create({
+      UserId:2,
+      TopicId:qid,
+      score:score,
+      createdAt:new Date,
+      updatedAt:new Date,
+    })
+  })
+  .then(data=>{
+    res.redirect("/quiz?result=" + score)
+  })
+  .catch(err=>{
+    res.send(err)
+  })
+  // res.send(req.body,req.params.id)
+  
+})
+
+app.post("/cname", (req, res) => {
+    Model.User.update({name: req.body.newname}, {
+        where: {
+            email: req.session.email
+        }
+    })
+    .then(() => {
+        res.render("print_msg.ejs", {
+            msg: `Successfully Updated Name for: ${req.session.email}`,
+            loggedin: req.session.loggedin,
+            email: req.session.email
+        })
+    })
+    .catch(err=>{
+        res.send(err)
+    })
+
+})
+
+app.post("/cpassword", (req, res) => {
+    Model.User.findOne({
+        where: {
+            email: req.session.email
+        }
+    })
+    .then(user => {
+        if(bcrypt.compareSync(req.body.oldpassword, user.password)) {
+            req.session.loggedin = true;
+
+            let newpass = {
+                password: req.body.newpassword
+            }
+
+            Model.User.update(newpass, {
+                where: {
+                    email: req.session.email
+                }
+            })
+            .then(() => {
+                res.render("print_msg.ejs", {
+                    msg: `Successfully Changed Password for: ${req.session.email}`,
+                    loggedin: req.session.loggedin,
+                    email: req.session.email
+                })
+            })
+            .catch(err=>{
+                res.send(err)
+            })
+        
+        }
+        else {
+            res.render("print_msg.ejs", {
+                msg: "Old password is incorrect",
+                loggedin: req.session.loggedin,
+                email: req.session.email
+            })
+        }
+    })  
+})
+
+app.post("/delete", (req, res) => {
+    Model.User.destroy({
+        where: {
+            email: req.session.email.toString()
+        }
+    })
+    .then(() => {
+        req.session.destroy();
+        res.render("print_msg.ejs", {
+            msg: "Account destroyed",
+            loggedin: false,
+            email: null
+        })
+    })
+    .catch(err=>{
+        res.send(err)
+    })
+})  
 
 app.listen(PORT, () => console.log(`Server started on port: ${PORT}`))
